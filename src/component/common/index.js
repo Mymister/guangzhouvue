961da5af106a83902664d428e01c0c0b9843d5a1
Mymister <804563996@qq.com>
2017-11-18 14:35:55 +0800

    集成mui组件库(mint-ui是头部，这个是底部)

--- src/component/common/index.js
+++ src/component/common/index.js
@@ -1,8 +1,9 @@
 import HeaderComponent from './header.vue';
-
+import FooterComponent from './footer.vue';
 
 export default{
     install(Vue){
         Vue.component('app-header',HeaderComponent);
+        Vue.component('app-footer',FooterComponent);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue';
 import MintUi from 'mint-ui';
 import 'mint-ui/lib/style.css';
 import Common from '../component/common';
-
+import 'mui/dist/css/mui.css';
+import 'mui/examples/hello-mui/css/icons-extra.css';
 
 Vue.use(MintUi);
 Vue.use(Common);
